943c4900d73c3311d29fbb0f1d5caaa33d45d84a
Muhammet Ali <94957965+MuhammetAli54@users.noreply.github.com>
2022-02-04 14:56:09 +0300

    PROJE TAMAMLANDI..

--- EcommerceLiteUI/Controllers/AdminController.cs
+++ EcommerceLiteUI/Controllers/AdminController.cs
@@ -7,11 +7,13 @@ using EcommerceLiteBLL.Repository;
 
 namespace EcommerceLiteUI.Controllers
 {
+
     [Authorize(Roles ="Admin")]
     public class AdminController : BaseController
     {
         //Global alan
         OrderRepo myOrderRepo = new OrderRepo();
+        CategoryRepo myCategoryRepo = new CategoryRepo();
         // GET: Admin
         public ActionResult DashBoard()
         {
@@ -21,6 +23,17 @@ namespace EcommerceLiteUI.Controllers
             ViewBag.NewOrderCount = newOrderCount;
 
             return View();
+        }
+
+        public ActionResult DashBoard2()
+        {
+            var orderList = myOrderRepo.GetAll();
+            //1 aylık sipariş sayısı
+            var newOrderCount = orderList.Where(x => x.RegisterDate >= DateTime.Now.AddMonths(-1)).ToList().Count();
+            ViewBag.NewOrderCount = newOrderCount;
+
+            var model = myCategoryRepo.GetBaseCategoriesProductCount();
+            return View(model);
         }
     }
 }
